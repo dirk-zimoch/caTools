847a642de824a1551863a2b693dd7a8521216637
Rok Vuga <rok.vuga@cosylab.com>
2016-02-01 18:43:24 +0100

    some caget functionality

--- caToolsApp/src/caToolsMain.c
+++ caToolsApp/src/caToolsMain.c
@@ -7,6 +7,7 @@
 #include <string.h>
 
 #include "cadef.h"  // for channel access
+#include "alarmString.h"  // XXX is ok?
 #include <getopt.h> // for getopt() epicsGetopt.h ???
 
 #include <stdbool.h>
@@ -43,28 +44,29 @@ enum roundType{
 
 struct arguments
 {
-   bool w;
-   int  d;
-   bool num;    // same as -int
+   float w;					//ca timeout
+   int  d;					//precision
+   bool num;    			//same as -int
    enum roundType round;
    int prec;
    bool hex;
    bool bin;
    bool oct;
    bool plain;
-   int digits;  // number of digits for e, f, g option. prec??
-   char dblFormatStr[30]; // Format string to print doubles (see -e -f -g option)
-   bool s;
+   int digits;  			// precision for e, f, g option
+   char dblFormatStr[30]; 	// Format string to print doubles (see -e -f -g option)
+   bool s;					//try to interpret values as strings
    char stat;
    bool noname;
    bool nounit;
 
-   int count;   // Request and print / write up to <num> elements.
+   int count;   			// Request and print / write up to <num> elements.
 };
 
-const struct arguments arguments_init = {
-    false,              // w
-    0,                  // d
+//const struct arguments arguments_init = {
+struct arguments arguments = {
+    CA_TIMEOUT,         // w
+    -1,                 // d
     false,              // num
     roundType_default,  // round
     -1,                 // prec
@@ -81,7 +83,7 @@ const struct arguments arguments_init = {
     0                   // count
 };
 
-struct arguments arguments = arguments_init;
+//struct arguments arguments = arguments_init;
 
 struct channel{
     char            *name;  // the name of the channel
@@ -98,7 +100,7 @@ void usage(FILE *stream, char *programName){
 
 void dumpArguments(struct arguments *args){
     printf("\n"
-           "CA timeout:\tw = %d\n"
+           "CA timeout:\tw = %f\n"
            "Requested DBR type:\td = %d\n"
            "Try to interpret values as numbers:\tnum = %d\n"
            "Round number to int:\tround = %d\n"
@@ -119,11 +121,13 @@ void dumpArguments(struct arguments *args){
            args->stat, args->noname, args->nounit);
 }
 
+
+/*rabimo to?? ne dela v C XXX
 void channelStatusCallback(connection_handler_args args){
     printf("Connection status changed:\n"
            "Channel id: %ld went %ld [6 = up\t7 = down]\n",
            (long)args.chid, args.op);
-}
+}*/
 
 int checkStatus(int status){
     if (status != ECA_NORMAL){
@@ -135,8 +139,30 @@ int checkStatus(int status){
     return EXIT_SUCCESS;
 }
 
-char* valueToString(unsigned int type, const void *dbr, int i){
-    char stringValue[100];    // TODO hmk??
+
+void sprintBits(char *outStr, size_t const size, void const * const ptr)
+//Converts a decimal number to binary and saves it to outStr.
+//The decimal number is pointed to by ptr and is of memory size size.
+//Assumes little endian.
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+    int k=0;
+
+    for (i=size-1;i>=0;i--)
+    {
+        for (j=7;j>=0;j--)
+        {
+            byte = b[i] & (1<<j);
+            byte >>= j;
+            sprintf(outStr + k, "%u", byte);
+            k++;
+        }
+    }
+}
+
+int valueToString(char *stringValue, unsigned int type, const void *dbr, int i){
     dbr_enum_t v;
     unsigned int baseType;
     void *value;
@@ -150,7 +176,18 @@ char* valueToString(unsigned int type, const void *dbr, int i){
             break;
 
         case DBR_INT:
-            sprintf(stringValue, "%d", ((dbr_int_t*)value)[i]);
+        	if (arguments.hex){
+        		sprintf(stringValue, "%x", ((dbr_int_t*)value)[i]);
+        	}
+        	else if (arguments.oct){
+        		sprintf(stringValue, "%o", ((dbr_int_t*)value)[i]);
+        	}
+        	else if (arguments.bin){
+        		sprintBits(stringValue, sizeof(((dbr_int_t*)value)[i]), &((dbr_int_t*)value)[i]);
+        	}
+        	else{
+        		sprintf(stringValue, "%d", ((dbr_int_t*)value)[i]);
+        	}
             break;
 
         //case DBR_SHORT: equals DBR_INT
@@ -158,7 +195,7 @@ char* valueToString(unsigned int type, const void *dbr, int i){
         //    break;
 
         case DBR_FLOAT:
-            sprintf(stringValue, "%f", ((dbr_float_t*)value)[i]);
+        	sprintf(stringValue, arguments.dblFormatStr, ((dbr_float_t*)value)[i]);
             break;
 
         case DBR_ENUM:
@@ -183,52 +220,71 @@ char* valueToString(unsigned int type, const void *dbr, int i){
             break;
 
         case DBR_LONG:
-            sprintf(stringValue, "%d", ((dbr_long_t*)value)[i]);
+        	if (arguments.hex){
+        		sprintf(stringValue, "%x", ((dbr_long_t*)value)[i]);
+        	}
+        	else if (arguments.oct){
+        		sprintf(stringValue, "%o", ((dbr_long_t*)value)[i]);
+        	}
+        	else if (arguments.bin){
+        		sprintBits(stringValue, sizeof(((dbr_long_t*)value)[i]), &((dbr_long_t*)value)[i]);
+        	}
+        	else{
+        		sprintf(stringValue, "%d", ((dbr_long_t*)value)[i]);
+        	}
             break;
 
         case DBR_DOUBLE:
-            sprintf(stringValue, "%f", ((dbr_double_t*)value)[i]);
+            sprintf(stringValue, arguments.dblFormatStr, ((dbr_double_t*)value)[i]);
             break;
 
         default:
             strcpy(stringValue, "Unrecognized DBR type");
     }
 
-    return stringValue;
+    return 0;
 }
+
 
 static void caCallback (evargs args){
     struct channel *ch = (struct channel *)args.usr;
     int i;
-    void *value;
-    char fieldSeparator = ' ';  // TODO get from arguments
+    char fieldSeparator = ' ';  // TODO get from arguments XXX ni zahtevano??
     int outNelm = -1;
 
-    ch->done = true;    // TODO should this be at the end?
-
-
-    // concat output
-    char timestamp[50], statusSeverity[2*30], units[20+MAX_UNITS_SIZE], ack[2*30];
+    // output strings
+#define LEN_TIMESTAMP 50
+#define LEN_RECORD_NAME 61
+    char timestamp[LEN_TIMESTAMP], severityStatus[2*30], units[20+MAX_UNITS_SIZE], ack[2*30], name[LEN_RECORD_NAME];
     //chsr precision[30], grLimits[6*45], ctrLimits[2*45], enums[30 + (MAX_ENUM_STATES * (20 + MAX_ENUM_STRING_SIZE))];
     int precision=-1;
+    int status=0, severity=0;
 
-    value = dbr_value_ptr(args.dbr, args.type);
+    sprintf(timestamp,"%s","");
+	sprintf(severityStatus,"%s","");
+	sprintf(units,"%s","");
+	sprintf(name,"%s","");
+	sprintf(ack,"%s","");
 
-#define severity_status_get(T) sprintf(statusSeverity, "(SEVR:%d STAT:%d)", ((struct T *)value)->status, ((struct T *)value)->severity);   // TODO should be text...
-#define timestamp_get(T) sprintf(timestamp, "%u.%u", ((struct T *)value)->stamp.secPastEpoch, ((struct T *)value)->stamp.nsec);   // TODO should be formatted...
-#define units_get(T) sprintf(units, "%s", ((struct T *)value)->units);
 
-    sprintf(timestamp," ");
-    sprintf(statusSeverity," ");
-    sprintf(units," ");
-    sprintf(ack," ");
+    //templates for reading requested data
+#define severity_status_get(T) \
+    status = ((struct T *)args.dbr)->status; \
+	severity = ((struct T *)args.dbr)->severity; \
+	sprintf(severityStatus, "%s%c%s", epicsAlarmSeverityStrings[severity], fieldSeparator, epicsAlarmConditionStrings[status]);
 
+#define timestamp_get(T) \
+		epicsTimeToStrftime(timestamp, LEN_TIMESTAMP, "%Y-%m-%d %H:%M:%S.%06f", (epicsTimeStamp *)&(((struct T *)args.dbr)->stamp.secPastEpoch));
+
+#define units_get(T) sprintf(units, "%s", ((struct T *)args.dbr)->units);
+
+    //read requested data
     switch (args.type) {
         case DBR_STS_STRING:
             severity_status_get(dbr_sts_string);
             break;
 
-        case DBR_STS_SHORT:   // and DBR_STS_INT
+        case DBR_STS_SHORT: //and INT
             severity_status_get(dbr_sts_short);
             break;
 
@@ -254,9 +310,10 @@ static void caCallback (evargs args){
 
         case DBR_TIME_STRING:
             severity_status_get(dbr_time_string);
+            timestamp_get(dbr_time_string);
             break;
 
-        //case DBR_TIME_INT: TODO ?
+        //case DBR_TIME_INT: not implemented, same as dbr short
         //    severity_status_get(dbr_time_);
         //    break;
 
@@ -302,7 +359,7 @@ static void caCallback (evargs args){
         case DBR_GR_FLOAT:
             severity_status_get(dbr_gr_float);
             units_get(dbr_gr_float);
-            precision = (int)(((struct dbr_gr_float *)value)->precision);
+            precision = (int)(((struct dbr_gr_float *)args.dbr)->precision);
             break;
 
         case DBR_GR_ENUM:
@@ -323,7 +380,7 @@ static void caCallback (evargs args){
         case DBR_GR_DOUBLE:
             severity_status_get(dbr_gr_double);
             units_get(dbr_gr_double);
-            precision = (int)(((struct dbr_gr_double *)value)->precision);
+            precision = (int)(((struct dbr_gr_double *)args.dbr)->precision);
             break;
 
         //case DBR_CTRL_STRING:
@@ -338,7 +395,7 @@ static void caCallback (evargs args){
         case DBR_CTRL_FLOAT:
             severity_status_get(dbr_ctrl_float);
             units_get(dbr_ctrl_float);
-            precision = (int)(((struct dbr_ctrl_float *)value)->precision);
+            precision = (int)(((struct dbr_ctrl_float *)args.dbr)->precision);
             break;
 
         case DBR_CTRL_ENUM:
@@ -359,21 +416,24 @@ static void caCallback (evargs args){
         case DBR_CTRL_DOUBLE:
             severity_status_get(dbr_ctrl_double);
             units_get(dbr_ctrl_double);
-            precision = (int)(((struct dbr_ctrl_double *)value)->precision);
+            precision = (int)(((struct dbr_ctrl_double *)args.dbr)->precision);
             break;
-        case DBR_STSACK_STRING: // do sutff here
+
+        case DBR_STSACK_STRING: // do sutff here XXX kaksen stuff
             severity_status_get(dbr_stsack_string);
-            sprintf(ack, "(ACKT:%d ACKS:%d)", ((struct dbr_stsack_string *)value)->ackt, ((struct dbr_stsack_string *)value)->acks);
+            sprintf(ack, "(ACKT:%d ACKS:%d)", ((struct dbr_stsack_string *)args.dbr)->ackt, ((struct dbr_stsack_string *)args.dbr)->acks);
             break;
 
         default :
             //strcpy (s, "Can not print %s DBR type", dbr_type_to_text(args.type));
             printf("Can not print %s DBR type", dbr_type_to_text(args.type));
-    } // TODO add the rest....
+            break;
+    } // TODO add the rest.... XXX kaksen rest
 
-    if (args.type >= DBR_CTRL_STRING ) {    // upper and lower control limits are available
+   /* XXX kaj nucajo tile
+	if (args.type >= DBR_CTRL_STRING ) {    // upper and lower control limits are available
 
-    }
+	}
 
     if (args.type >= DBR_GR_STRING ) {      // egu, precision, upper and lower value, alarm and display limits are available
 
@@ -385,20 +445,53 @@ static void caCallback (evargs args){
 
     if (args.type >= DBR_STS_STRING ) {     // status and severity are available
 
+    }*/
+
+
+    //construct output
+    //check arguments
+    if (!arguments.noname){
+    	sprintf(name, ch->name);
     }
 
+    if (arguments.nounit){
+    	sprintf(units,"%s","");
+    }
 
-    printf( "%s %s ", timestamp, ch->name);
+    if (arguments.stat == 0){//display if alarm
+    	if (status == 0 && severity == 0){
+        	sprintf(severityStatus,"%s","");
+    	}
+    }
+    else if (arguments.stat == 1){//always display
+    	//do nothing
+    }
+    else if (arguments.stat == 2){//never display
+    	sprintf(severityStatus, "%s","");
+    }
 
+    //date, time, channel name
+    printf( "%s%c%s%c", timestamp, fieldSeparator, name, fieldSeparator);
 
-    if (outNelm > 0 && outNelm < args.count) args.count = outNelm;
+    //values
+    if (outNelm > 0 && outNelm < args.count) args.count = outNelm; //TODO outNelm = zahtevano stevilo elementov
+
+    char valueStr[100];//XXX max size of output string?hmk??
+	sprintf(valueStr,"%s","");
+
     for(i=0; i < args.count; i++){
-        if (i) printf ("%c", fieldSeparator);
-        printf("%s", valueToString(args.type, args.dbr, i));
+    	if (i) printf ("%c", fieldSeparator);
+
+    	valueToString(valueStr + strlen(valueStr), args.type, args.dbr, i);
     }
+	printf("%s", valueStr);
 
 
-    printf( "%s %s\n", units, statusSeverity);
+    //egu, severity, status
+    printf( "%c%s%c%s\n", fieldSeparator, units, fieldSeparator, severityStatus);
+
+    //finish
+    ch->done = true;
 }
 
 
@@ -417,7 +510,7 @@ int caInit(struct channel *channels, int nChannels){
         if(checkStatus(status) != EXIT_SUCCESS) return EXIT_FAILURE;    // we exit if the channels are not created
     }
 
-    status = ca_pend_io ( CA_TIMEOUT ); // will wait until channels connect because create channel is not using a callback
+    status = ca_pend_io ( arguments.w ); // will wait until channels connect because create channel is not using a callback
     if(status == ECA_TIMEOUT){
         printf ("Some of the channels did not connect\n");
     }
@@ -436,8 +529,17 @@ void caRequest(struct channel *channels, int nChannels){
         if(arguments.count > 0 && arguments.count < channels[i].count) channels[i].count = arguments.count;
 
         channels[i].done = false;
-        channels[i].type = DBR_CTRL_DOUBLE; // TODO fix fix fix
-        status = ca_array_get_callback(channels[i].type, channels[i].count, channels[i].id, caCallback, &channels[i]);
+        if (arguments.d == -1){
+        	//if DBR type not specified, use native.
+        	//default: GR
+        	channels[i].type = dbf_type_to_DBR_GR(ca_field_type(channels[i].id));
+        	//TODO if only time and/or severity&status, use DBR_TIME_native
+        	//TODO if more, make 2 requests: first time, then GBR???
+        }
+        else{
+        	channels[i].type = arguments.d;
+        }
+       	status = ca_array_get_callback(channels[i].type, channels[i].count, channels[i].id, caCallback, &channels[i]);
         if (status == ECA_DISCONN) {
             printf("Channel %s is unable to connect.\n", channels[i].name);
         }
@@ -462,7 +564,7 @@ void caRequest(struct channel *channels, int nChannels){
         // TODO add argument -silent
     }
 
-    status = ca_pend_io ( CA_TIMEOUT ); // will wait until channels connect (create channel is without callback)
+    status = ca_pend_io ( arguments.w ); // will wait until channels connect (create channel is without callback)
     if(status == ECA_TIMEOUT){
         printf ("Some of the channels are not connected\n");
     }
@@ -503,22 +605,25 @@ int main ( int argc, char ** argv )
     static struct option long_options[] = {
         {"num",     no_argument,        0,  0 },
         {"int",     no_argument,        0,  0 },    // same as num
-        {"round",   optional_argument,  0,  0 },
-        {"prec",    required_argument,  0,  0 },
-        {"hex",     no_argument,        0,  0 },
-        {"bin",     no_argument,        0,  0 },
-        {"oct",     no_argument,        0,  0 },
-        {"plain",   no_argument,        0,  0 },
-        {"stat",    required_argument,  0,  0 },
-        {"noname",  no_argument,        0,  0 },
-        {"nounit",  no_argument,        0,  0 },
+        {"round",   optional_argument,  0,  0 },	//type of rounding:default, ceil, floor
+        {"prec",    required_argument,  0,  0 },	//precision
+        {"hex",     no_argument,        0,  0 },	//display as hex
+        {"bin",     no_argument,        0,  0 },	//display as bin
+        {"oct",     no_argument,        0,  0 },	//display as oct
+        {"plain",   no_argument,        0,  0 },	//ignore formatting switches
+        {"stat",    required_argument,  0,  0 },	//status and severity XXX???
+        {"noname",  no_argument,        0,  0 },	//hide name
+        {"nounit",  no_argument,        0,  0 },	//hide units
         {0,         0,                  0,  0 }
-    };
+    };	//TODO COUNT!
     // TODO mutually exclusive
-    while ((opt = getopt_long_only(argc, argv, "wd:e:f:g:sh", long_options, &opt_long)) != -1) {
+    while ((opt = getopt_long_only(argc, argv, "w:d:e:f:g:sh", long_options, &opt_long)) != -1) {
         switch (opt) {
         case 'w':
-            arguments.w = true;
+        	if (sscanf(optarg, "%f", &arguments.w) != 1){    // type was not given as float
+        		arguments.w = CA_TIMEOUT;
+        		fprintf(stderr, "Requested timeout invalid - ignored. ('%s -h' for help.)\n", argv[0]);
+        	}
             break;
         case 'd':
             if (sscanf(optarg, "%d", &arguments.d) != 1)    // type was not given as an integer
@@ -539,7 +644,7 @@ int main ( int argc, char ** argv )
                 arguments.d = -1;
             }
             break;
-        case 'e':
+        case 'e':	//how to show format doubles in strings
         case 'f':
         case 'g':
             if (sscanf(optarg, "%d", &arguments.digits) != 1){
@@ -555,7 +660,7 @@ int main ( int argc, char ** argv )
                 arguments.digits = -1;
             }
             break;
-        case 's':
+        case 's':	//try to interpret values as strings
             arguments.s = true;
             break;
         case 0:   // long options
@@ -567,6 +672,7 @@ int main ( int argc, char ** argv )
                 arguments.num = true;
                 break;
             case 2:   // round
+            	;//needs to precede declaration because of C
                 int type;
                 if (sscanf(optarg, "%d", &type) != 1){   // type was not given as a number [0, 1, 2]
                     if(!strcmp("default", optarg)){
@@ -588,7 +694,8 @@ int main ( int argc, char ** argv )
                             "Invalid round type '%s' "
                             "for option '-%c' - ignored.\n", optarg, opt);
                     } else{
-                        arguments.round = (roundType)type;
+                        //arguments.round = (roundType)type;//casting causes error?? XXX
+                    	arguments.round = type;
                     }
                 }
                 break;
@@ -606,7 +713,7 @@ int main ( int argc, char ** argv )
                 }
                 break;
             case 4:   //hex
-                arguments.hex = true;
+                arguments.hex = true; //TODO warning if these requested for non integer type
                 break;
             case 5:   // bin
                 arguments.bin = true;
