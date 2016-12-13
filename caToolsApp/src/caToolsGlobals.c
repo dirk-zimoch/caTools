1a1d6cbbb5394488978d60f3c2e5f0f12d6d624b
Tom Slejko <tom.slejko@cosylab.com>
2016-12-13 10:27:10 +0100

    Lower verbosity level to ERR

--- caToolsApp/src/caToolsGlobals.c
+++ caToolsApp/src/caToolsGlobals.c
@@ -77,4 +77,4 @@ epicsTimeStamp g_timeoutTime;       /* when to stop monitoring (-timeout) */
 
 bool g_runMonitor;                /* indicates when to stop monitoring according to -timeout, -n or cawait condition is true */
 u_int32_t g_numMonitorUpdates;    /* counts updates needed by -n */
-int g_verbosity = VERBOSITY_WARN;  /* global verbosity */
+int g_verbosity = VERBOSITY_ERR;  /* global verbosity */
